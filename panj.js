37c5d419365267a2d6b24e8bb639df7bc163d6f4
Ipanskii <mirfanmbrk@gmail.com>
2026-03-04 13:20:38 +0700

    Add files via upload

---
+++ panj.js
@@ -0,0 +1,6 @@
+// Form submit handler
+document.getElementById("contactForm").addEventListener("submit", function(e) {
+  e.preventDefault();
+  alert("Terima kasih! Pesan Anda telah terkirim.");
+  this.reset();
+});
